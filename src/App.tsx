757f057ec7eb2387d60c6b642e55d188d6ed64b9
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-03-27 02:14:53 +0200

    Add recenter button, implement initial rendering in center

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View, useWindowDimensions } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import TestGraph from './components/graphs/TestGraph';
 import PannableScalableView from './views/PannableScalableView';
 
 function App() {
+  const { height } = useWindowDimensions();
+
   return (
     <SafeAreaView className='grow'>
       <GestureHandlerRootView className='grow'>
-        <PannableScalableView>
-          <TestGraph />
-        </PannableScalableView>
+        <View style={{ height: height / 2 }} className='bg-slate-600'>
+          <PannableScalableView className='w-20' controls>
+            <TestGraph />
+          </PannableScalableView>
+        </View>
       </GestureHandlerRootView>
     </SafeAreaView>
   );
--- src/views/PannableScalableView.tsx
+++ src/views/PannableScalableView.tsx
@@ -1,6 +1,6 @@
 import { styled } from 'nativewind';
-import { Children, PropsWithChildren, cloneElement } from 'react';
-import { useWindowDimensions } from 'react-native';
+import { Children, PropsWithChildren, cloneElement, useCallback } from 'react';
+import { LayoutChangeEvent, View } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import {
   Easing,
@@ -12,32 +12,38 @@ import {
 
 import { Canvas, Group } from '@shopify/react-native-skia';
 
-import { RenderEvent } from '../components/graphs/TestGraph';
+import ViewControls from '@/components/controls/ViewControls';
+import {
+  MeasureEvent,
+  TestGraphPrivateProps
+} from '@/components/graphs/TestGraph';
+import { Dimensions, ObjectFit } from '@/types/views.types';
+import { getCenterInParent, getScaleInParent } from '@/utils/views.utils';
 
 const StyledCanvas = styled(Canvas, 'grow');
 
 type PannableScalableViewProps = PropsWithChildren<{
   minScale?: number; // default is auto (when the whole content is visible)
   maxScale?: number;
-  defaultScale?: number;
+  objectFit?: ObjectFit;
   className?: string;
+  controls?: boolean;
 }>;
 
 export default function PannableScalableView({
   minScale = 0.25, // TODO
   maxScale = 10, // TODO
-  defaultScale = 1, // TODO
+  objectFit = 'none',
   className,
-  children
+  children,
+  controls = false
 }: PannableScalableViewProps) {
-  // TODO - use dimensions from props or calculate dimensions dynamically (render canvas with flex grow, then measure canvas dimensions)
-  const { height: screenHeight, width: screenWidth } = useWindowDimensions();
-  const graphWidth = useSharedValue(0);
-  const graphHeight = useSharedValue(0);
+  const canvasDimensions = useSharedValue({ width: 0, height: 0 });
+  const contentDimensions = useSharedValue({ width: 0, height: 0 });
+  const initialScale = useSharedValue(0);
 
-  const startScale = useSharedValue(defaultScale);
-  const scale = useSharedValue(defaultScale);
-
+  const startScale = useSharedValue(0);
+  const scale = useSharedValue(0);
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
 
@@ -50,9 +56,79 @@ export default function PannableScalableView({
     [translateX, translateY, scale]
   );
 
+  const handleCanvasRender = useCallback(
+    ({
+      nativeEvent: {
+        layout: { width, height }
+      }
+    }: LayoutChangeEvent) => {
+      canvasDimensions.value = { width, height };
+      updateContentPosition(contentDimensions.value, { width, height });
+    },
+    []
+  );
+
+  const handleContentMeasure = useCallback(
+    ({ layout: { width, height } }: MeasureEvent) => {
+      contentDimensions.value = { width, height };
+      updateContentPosition({ width, height }, canvasDimensions.value);
+    },
+    []
+  );
+
+  // TODO - fx centering not always working (race condition?)
+  const updateContentPosition = useCallback(
+    (contentDims: Dimensions, canvasDims: Dimensions, animated?: boolean) => {
+      const { scale: renderedScale, dimensions: renderedDimensions } =
+        getScaleInParent(objectFit, contentDims, canvasDims);
+
+      initialScale.value = renderedScale;
+      scaleContentTo(renderedScale, undefined, animated);
+
+      translateContentTo(
+        getCenterInParent(renderedDimensions, canvasDims),
+        undefined,
+        animated
+      );
+    },
+    [objectFit]
+  );
+
+  const handleReset = useCallback(() => {
+    updateContentPosition(
+      contentDimensions.value,
+      canvasDimensions.value,
+      true
+    );
+  }, []);
+
   const clamp = (value: number, bounds: [number, number]) => {
     'worklet';
     return Math.min(Math.max(value, bounds[0]), bounds[1]);
+  };
+
+  const translateContentTo = (
+    translate: { x: number; y: number },
+    clampTo?: { x?: [number, number]; y?: [number, number] },
+    animated?: boolean
+  ) => {
+    'worklet';
+    const timingConfig = { duration: 150, easing: Easing.ease };
+
+    const newTranslateX = clampTo?.x
+      ? clamp(translate.x, clampTo.x)
+      : translate.x;
+    const newTranslateY = clampTo?.y
+      ? clamp(translate.y, clampTo.y)
+      : translate.y;
+
+    if (animated) {
+      translateX.value = withTiming(newTranslateX, timingConfig);
+      translateY.value = withTiming(newTranslateY, timingConfig);
+    } else {
+      translateX.value = newTranslateX;
+      translateY.value = newTranslateY;
+    }
   };
 
   const scaleContentTo = (
@@ -66,28 +142,29 @@ export default function PannableScalableView({
 
     if (origin) {
       const relativeScale = clampedScale / scale.value;
-      const newTranslateX = clamp(
-        translateX.value - (origin.x - translateX.value) * (relativeScale - 1),
-        [
-          Math.min(0, screenWidth - graphWidth.value * clampedScale),
-          Math.max(0, screenWidth - graphWidth.value * clampedScale)
-        ]
-      );
-      const newTranslateY = clamp(
-        translateY.value - (origin.y - translateY.value) * (relativeScale - 1),
-        [
-          Math.min(0, screenHeight - graphHeight.value * clampedScale),
-          Math.max(0, screenHeight - graphHeight.value * clampedScale)
-        ]
-      );
+      const { width: contentWidth, height: contentHeight } =
+        contentDimensions.value;
+      const { width: canvasWidth, height: canvasHeight } =
+        canvasDimensions.value;
 
-      if (animated) {
-        translateX.value = withTiming(newTranslateX, timingConfig);
-        translateY.value = withTiming(newTranslateY, timingConfig);
-      } else {
-        translateX.value = newTranslateX;
-        translateY.value = newTranslateY;
-      }
+      const clampWidth = canvasWidth - contentWidth * clampedScale;
+      const clampHeight = canvasHeight - contentHeight * clampedScale;
+
+      translateContentTo(
+        {
+          x:
+            translateX.value -
+            (origin.x - translateX.value) * (relativeScale - 1),
+          y:
+            translateY.value -
+            (origin.y - translateY.value) * (relativeScale - 1)
+        },
+        {
+          x: [Math.min(0, clampWidth), Math.max(0, clampWidth)],
+          y: [Math.min(0, clampHeight), Math.max(0, clampHeight)]
+        },
+        animated
+      );
     }
 
     if (animated) {
@@ -97,31 +174,32 @@ export default function PannableScalableView({
     }
   };
 
-  const panGesture = Gesture.Pan()
+  const panGestureHandler = Gesture.Pan()
     .onChange(e => {
       translateX.value += e.changeX;
       translateY.value += e.changeY;
     })
     .onEnd(e => {
+      const clampWidth =
+        canvasDimensions.value.width -
+        contentDimensions.value.width * scale.value;
+      const clampHeight =
+        canvasDimensions.value.height -
+        contentDimensions.value.height * scale.value;
+
       translateX.value = withDecay({
         velocity: e.velocityX,
-        clamp: [
-          Math.min(0, screenWidth - graphWidth.value * scale.value),
-          Math.max(0, screenWidth - graphWidth.value * scale.value)
-        ],
+        clamp: [Math.min(0, clampWidth), Math.max(0, clampWidth)],
         rubberBandEffect: true
       });
       translateY.value = withDecay({
         velocity: e.velocityY,
-        clamp: [
-          Math.min(0, screenHeight - graphHeight.value * scale.value),
-          Math.max(0, screenHeight - graphHeight.value * scale.value)
-        ],
+        clamp: [Math.min(0, clampHeight), Math.max(0, clampHeight)],
         rubberBandEffect: true
       });
     });
 
-  const pinchGesture = Gesture.Pinch()
+  const pinchGestureHandler = Gesture.Pinch()
     .onStart(() => {
       startScale.value = scale.value;
     })
@@ -146,36 +224,37 @@ export default function PannableScalableView({
       const origin = { x, y };
       const halfScale = (maxScale + minScale) / 2;
 
-      if (scale.value < defaultScale) {
-        scaleContentTo(defaultScale, origin);
+      if (scale.value < initialScale.value) {
+        scaleContentTo(initialScale.value, origin);
       } else if (scale.value < halfScale) {
         scaleContentTo(halfScale, origin);
       } else if (scale.value < maxScale) {
         scaleContentTo(maxScale, origin);
       } else {
-        scaleContentTo(defaultScale, origin);
+        scaleContentTo(initialScale.value, origin);
       }
     });
 
   return (
-    <GestureDetector
-      gesture={Gesture.Race(
-        Gesture.Simultaneous(pinchGesture, panGesture),
-        tapGestureHandler
-      )}>
-      <StyledCanvas className={className}>
-        <Group transform={transform}>
-          {Children.map(children, child => {
-            const childElement = child as React.ReactElement;
-            return cloneElement(childElement, {
-              onRender: ({ layout }: RenderEvent) => {
-                graphWidth.value = layout.width.value;
-                graphHeight.value = layout.height.value;
-              }
-            });
-          })}
-        </Group>
-      </StyledCanvas>
-    </GestureDetector>
+    <View className='grow relative'>
+      <GestureDetector
+        gesture={Gesture.Race(
+          Gesture.Simultaneous(pinchGestureHandler, panGestureHandler),
+          tapGestureHandler
+        )}>
+        <StyledCanvas className={className} onLayout={handleCanvasRender}>
+          <Group transform={transform}>
+            {Children.map(children, child => {
+              const childElement =
+                child as React.ReactElement<TestGraphPrivateProps>;
+              return cloneElement(childElement, {
+                onMeasure: handleContentMeasure
+              });
+            })}
+          </Group>
+        </StyledCanvas>
+      </GestureDetector>
+      {controls && <ViewControls onReset={handleReset} />}
+    </View>
   );
 }
