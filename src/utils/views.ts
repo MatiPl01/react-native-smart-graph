7fa1116b4598b2f50c9340c881b2ddd6e7e89238
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-04-16 12:50:45 +0200

    Add container scaling

--- src/utils/views.ts
+++ src/utils/views.ts
@@ -1,58 +1,31 @@
-import { Dimensions, Position } from '@/types/layout';
+import { Dimensions } from '@/types/layout';
 import { ObjectFit } from '@/types/views';
 
 export const getScaleInParent = (
   objectFit: ObjectFit,
   containerDimensions: Dimensions,
   parentDimensions: Dimensions
-): { scale: number; dimensions: Dimensions } => {
+): number => {
   const { width: containerWidth, height: containerHeight } =
     containerDimensions;
   const { width: parentWidth, height: parentHeight } = parentDimensions;
-
-  let scale = 1;
 
   switch (objectFit) {
     case 'contain':
-      scale = Math.min(
+      return Math.min(
         parentWidth / containerWidth,
         parentHeight / containerHeight
       );
-      break;
     case 'cover':
-      scale = Math.max(
+      return Math.max(
         parentWidth / containerWidth,
         parentHeight / containerHeight
       );
-      break;
     case 'none':
-      scale = 1;
-      break;
+      return 1;
     default:
-      scale = isNaN(objectFit) ? 1 : objectFit;
+      return isNaN(objectFit) ? 1 : objectFit;
   }
-
-  return {
-    scale,
-    dimensions: {
-      width: containerWidth * scale,
-      height: containerHeight * scale
-    }
-  };
-};
-
-export const getCenterInParent = (
-  containerDimensions: Dimensions,
-  parentDimensions: Dimensions
-): Position => {
-  const { width: containerWidth, height: containerHeight } =
-    containerDimensions;
-  const { width: parentWidth, height: parentHeight } = parentDimensions;
-
-  return {
-    x: (parentWidth - containerWidth) / 2,
-    y: (parentHeight - containerHeight) / 2
-  };
 };
 
 export const clamp = (value: number, bounds: [number, number]) => {
