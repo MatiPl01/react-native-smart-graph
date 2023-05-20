6a836669077def77ec322800c38970a0661d08e5
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-05-20 10:53:09 +0200

    Remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ export default function App() {
                   },
                   components: {
                     edge: {
-                      type: 'straight' // TODO - fix types
+                      type: 'straight'
                     }
                   }
                 }}
