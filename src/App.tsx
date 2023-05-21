00784e5fa4daad58783ee278d3f56869a0386c44
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-05-21 14:07:20 +0200

    Start working on batch graph updates

--- src/App.tsx
+++ src/App.tsx
@@ -161,7 +161,7 @@ export default function App() {
                   },
                   components: {
                     edge: {
-                      type: 'curved'
+                      type: 'straight'
                     }
                   }
                 }}
