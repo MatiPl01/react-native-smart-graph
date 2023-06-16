3e26fa3f94cf665a388c519a340d4aefa05da860
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-06-16 09:11:21 +0200

    Create placement layout provider, fix some issues (#119)

--- src/providers/layout/PlacementLayoutProvider.tsx
+++ src/providers/layout/PlacementLayoutProvider.tsx
@@ -1,4 +1,5 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useMemo } from 'react';
+import { useAnimatedReaction } from 'react-native-reanimated';
 
 import { withGraphData } from '@/providers/ComponentsDataProvider';
 import {
@@ -6,7 +7,13 @@ import {
   VertexComponentRenderData
 } from '@/types/components';
 import { Graph } from '@/types/graphs';
-import { GraphSettingsWithDefaults } from '@/types/settings';
+import {
+  AnimationSettingsWithDefaults,
+  GraphLayout,
+  GraphSettingsWithDefaults
+} from '@/types/settings';
+import { animateVerticesToFinalPositions } from '@/utils/animations';
+import { placeVertices } from '@/utils/placement';
 
 export type GraphPlacementLayoutProviderProps<V, E> = PropsWithChildren<{
   // Component props
@@ -15,18 +22,59 @@ export type GraphPlacementLayoutProviderProps<V, E> = PropsWithChildren<{
   // Injected props
   verticesRenderData: Record<string, VertexComponentRenderData>;
   edgesRenderData: Record<string, EdgeComponentRenderData>;
+  layoutAnimationSettings: AnimationSettingsWithDefaults;
 }>;
 
 function GraphPlacementLayoutProvider<V, E>({
+  graph,
+  settings,
+  verticesRenderData,
+  edgesRenderData,
+  layoutAnimationSettings,
   children
 }: GraphPlacementLayoutProviderProps<V, E>) {
+  const graphLayout = useMemo<GraphLayout>(
+    () =>
+      placeVertices(
+        graph,
+        settings.components.vertex.radius,
+        settings.placement
+      ),
+    [
+      verticesRenderData,
+      edgesRenderData,
+      settings.components.vertex.radius,
+      settings.placement
+    ]
+  );
+
+  useAnimatedReaction(
+    () => ({
+      finalPositions: graphLayout.verticesPositions
+    }),
+    ({ finalPositions }) => {
+      animateVerticesToFinalPositions(
+        Object.fromEntries(
+          Object.entries(verticesRenderData).map(([key, { position }]) => [
+            key,
+            position
+          ])
+        ),
+        finalPositions,
+        layoutAnimationSettings
+      );
+    },
+    [graphLayout]
+  );
+
   return <>{children}</>;
 }
 
 export default withGraphData(
   GraphPlacementLayoutProvider,
-  ({ verticesRenderData, edgesRenderData }) => ({
+  ({ verticesRenderData, edgesRenderData, layoutAnimationSettings }) => ({
     verticesRenderData,
-    edgesRenderData
+    edgesRenderData,
+    layoutAnimationSettings
   })
 );
