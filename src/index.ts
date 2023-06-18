24d5264688c4e45294e101fb5055bf01bed2a7c4
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-06-18 10:22:57 +0200

    Add blank lines in src/index file

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,18 @@
 // COMPONENTS
 export { default as DirectedGraphComponent } from '@/components/graphs/DirectedGraphComponent';
 export { default as UndirectedGraphComponent } from '@/components/graphs/UndirectedGraphComponent';
+
 // RENDERERS
 export { default as DefaultEdgeArrowRenderer } from '@/components/graphs/arrows/renderers/DefaultEdgeArrowRenderer';
 export { default as DefaultCurvedEdgeRenderer } from '@/components/graphs/edges/curved/renderers/DefaultCurvedEdgeRenderer';
 export { default as DefaultStraightEdgeRenderer } from '@/components/graphs/edges/straight/renderers/DefaultStraightEdgeRenderer';
 export { default as DefaultEdgeLabelRenderer } from '@/components/graphs/labels/renderers/DefaultEdgeLabelRenderer';
 export { default as DefaultVertexRenderer } from '@/components/graphs/vertices/renderers/DefaultVertexRenderer';
+
 // MODELS
 export { default as DirectedGraph } from '@/models/graphs/DirectedGraph';
 export { default as UndirectedGraph } from '@/models/graphs/UndirectedGraph';
+
 // PROVIDERS
 export { default as GraphEventsProvider } from '@/providers/events';
 
@@ -26,5 +29,3 @@ export { default as PannableScalableView } from '@/views/PannableScalableView';
 
 // EXAMPLES
 export * from '@/examples';
-
-// TODO - export types
