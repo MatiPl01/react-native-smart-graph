c06d8f0cb601644110342c24d2491d70c6583d5c
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-06-27 20:36:12 +0200

    Fix typescript types in npm package, upgrade libs

--- babel.config.cjs.js
+++ babel.config.cjs.js
@@ -3,22 +3,8 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        modules: 'commonjs',
-        useBuiltIns: false,
         targets: {
-          browsers: [
-            '>1%',
-            'last 2 chrome versions',
-            'last 2 edge versions',
-            'last 2 firefox versions',
-            'last 2 safari versions',
-            'not dead',
-            'not ie <= 11',
-            'not op_mini all',
-            'not android <= 4.4',
-            'not samsung <= 4'
-          ],
-          node: '16'
+          node: 'current'
         }
       }
     ],
