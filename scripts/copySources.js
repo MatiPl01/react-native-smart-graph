47be652ed74af4bfc6e073d58dc2d131dcaba60f
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-09-10 16:16:48 +0200

    Fix all sources issues

--- scripts/copySources.js
+++ scripts/copySources.js
@@ -3,12 +3,15 @@ const path = require('path');
 const glob = require('glob');
 
 // Assuming tsconfig.json is in the root folder
-const tsconfig = JSON.parse(fs.readFileSync('./tsconfig.json'));
+const tsconfig = JSON.parse(fs.readFileSync('./tsconfig.json', 'utf8'));
 const basePath = path.resolve(tsconfig.compilerOptions.baseUrl);
 
 const aliases = Object.entries(tsconfig.compilerOptions.paths).reduce(
   (acc, [alias, values]) => {
-    acc[alias] = path.resolve(basePath, values[0].replace('*', '')); // Remove wildcard
+    acc[alias.replaceAll('/', '//')] = path.resolve(
+      basePath,
+      values[0].replace('*', '') // Remove wildcard
+    );
     return acc;
   },
   {}
@@ -18,19 +21,25 @@ const allFiles = glob.sync('./src/**/*.*');
 
 allFiles.forEach(file => {
   let content = fs.readFileSync(file, 'utf8');
-
   const target = `./dist/${file}`;
 
   // Replace aliases only for .ts and .tsx files
   if (file.endsWith('.ts') || file.endsWith('.tsx')) {
     Object.entries(aliases).forEach(([alias, value]) => {
-      const regex = new RegExp(`(['"])${alias}/`, 'g');
-      let relativePath = path.relative(path.dirname(file), value);
-      relativePath = relativePath.startsWith('.')
-        ? relativePath
-        : `./${relativePath}`;
+      const regex = new RegExp(`(['"])${alias}(.*?)['"]`, 'g'); // capture potential submodule
 
-      content = content.replace(regex, `$1${relativePath}/`);
+      content = content.replace(regex, (match, quote, submodule) => {
+        let relativePath = path.relative(
+          path.dirname(file),
+          path.resolve(basePath, value, submodule)
+        );
+
+        if (!relativePath.startsWith('.')) {
+          relativePath = './' + relativePath;
+        }
+
+        return `${quote}${relativePath}${quote}`;
+      });
     });
   }
 
