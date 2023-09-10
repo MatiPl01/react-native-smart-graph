37fa53d5715333af854c248d4c489274b7a39a32
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-09-10 17:16:18 +0200

    Remove bounding rect

--- src/components/graphs/graph/GraphComponent.tsx
+++ src/components/graphs/graph/GraphComponent.tsx
@@ -1,6 +1,5 @@
-import { Rect } from '@shopify/react-native-skia';
 import { memo } from 'react';
-import { useDerivedValue, useSharedValue } from 'react-native-reanimated';
+import { useSharedValue } from 'react-native-reanimated';
 
 import { useComponentFocus } from '@/hooks/focus';
 import { useCanvasContexts } from '@/providers/graph/contexts';
@@ -12,28 +11,15 @@ import GraphVertices from './GraphVertices';
 function GraphComponent() {
   // CONTEXTS
   // Canvas contexts
-  const {
-    dataContext: { boundingRect },
-    focusContext
-  } = useCanvasContexts();
+  const { focusContext } = useCanvasContexts();
 
   // A helper value to animate components on vertex focus
   const focusProgress = useSharedValue(0);
   // Update the focusProgress
   useComponentFocus(focusProgress, focusContext);
 
-  const x = useDerivedValue(() => boundingRect.left.value);
-  const y = useDerivedValue(() => boundingRect.top.value);
-  const w = useDerivedValue(
-    () => boundingRect.right.value - boundingRect.left.value
-  );
-  const h = useDerivedValue(
-    () => boundingRect.bottom.value - boundingRect.top.value
-  );
-
   return (
     <>
-      <Rect color='#333' height={h} width={w} x={x} y={y} />
       {/* TODO - maybe remove the mask component - it degrades performance */}
       {/* <Mask
         mask={<GraphEdgesMask boundingRect={boundingRect} />}
