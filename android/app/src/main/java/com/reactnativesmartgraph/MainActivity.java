5eaf593f2109560986d41cb3e056d1065d14a33b
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-09-16 20:36:50 +0200

    chore: Library build and name fixes (#350)
    * Add patched reanimated as a dependency
    
    * Fix commonjs exports issue
    
    * Fix commonjs babel config issue
    
    * Fix sources generation issues
    
    * Fix all sources issues
    
    * Remove bounding rect
    
    * chore: Rename library files (#349)
    
    * Rename library files
    
    * Remove generated documentation directory

--- android/app/src/main/java/com/reactnativesmartgraph/MainActivity.java
+++ android/app/src/main/java/com/reactnativesmartgraph/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativeforcegraph;
+package com.reactnativesmartgraph;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ReactNativeForceGraph";
+    return "ReactNativeSmartGraph";
   }
 
   /**
--- android/app/src/main/java/com/reactnativesmartgraph/MainApplication.java
+++ android/app/src/main/java/com/reactnativesmartgraph/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativeforcegraph;
+package com.reactnativesmartgraph;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
