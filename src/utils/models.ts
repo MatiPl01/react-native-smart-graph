27df2225eea739b9bbc40177601107b040333ede
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-10-08 15:44:12 +0200

    Add a possibility to get graph data

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -1,3 +1,7 @@
+import { DirectedEdgeData, UndirectedEdgeData, VertexData } from '@/types/data';
+import { DirectedEdge, UndirectedEdge, Vertex } from '@/types/models';
+import { WithValue } from '@/types/utils';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export type ChangeResult<T = void> = {
   data?: T;
@@ -32,3 +36,42 @@ export const catchError =
       };
     }
   };
+
+export const hasValue = <T extends object, V>(
+  obj: T | WithValue<V, T>
+): obj is WithValue<V, T> => Object.hasOwn(obj, 'value');
+
+export const getVertexData = <V, E>(vertex: Vertex<V, E>): VertexData<V> =>
+  (hasValue(vertex)
+    ? { key: vertex.key, value: vertex.value }
+    : { key: vertex.key }) as VertexData<V>;
+
+export const getDirectedEdgeData = <V, E>(
+  edge: DirectedEdge<V, E>
+): DirectedEdgeData<E> =>
+  (hasValue(edge)
+    ? {
+        from: edge.source.key,
+        key: edge.key,
+        to: edge.target.key,
+        value: edge.value
+      }
+    : {
+        from: edge.source.key,
+        key: edge.key,
+        to: edge.target.key
+      }) as DirectedEdgeData<E>;
+
+export const getUndirectedEdgeData = <V, E>(
+  edge: UndirectedEdge<V, E>
+): UndirectedEdgeData<E> =>
+  (hasValue(edge)
+    ? {
+        key: edge.key,
+        value: edge.value,
+        vertices: edge.vertices.map(({ key }) => key)
+      }
+    : {
+        key: edge.key,
+        vertices: edge.vertices.map(({ key }) => key)
+      }) as UndirectedEdgeData<E>;
